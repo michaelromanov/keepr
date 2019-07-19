3bf34beb13f874c3ec44fa0fd38d0ca2cd89a150
michaelromanov <michael.alexander.romanov@gmail.com>
2019-07-19 17:04:16 -0600

    commit

--- Startup.cs
+++ Startup.cs
@@ -48,7 +48,8 @@ namespace Keepr
                     {
                         builder
                             .WithOrigins(new string[]{
-                                "http://localhost:8080"
+                                "http://localhost:8080", 
+                                "http://localhost:8081"
                             })
                             .AllowAnyMethod()
                             .AllowAnyHeader()
