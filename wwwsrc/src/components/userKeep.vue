c80c1539303b5c6e1f4133ab296da1cc5bf16db7
michaelromanov <michael.alexander.romanov@gmail.com>
2019-07-25 14:32:48 -0600

    Begin Vaults

--- wwwsrc/src/components/userKeep.vue
+++ wwwsrc/src/components/userKeep.vue
@@ -34,4 +34,17 @@ methods: {
 }
 </script>
 
-<style></style>
+<style scoped>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+
+</style>
